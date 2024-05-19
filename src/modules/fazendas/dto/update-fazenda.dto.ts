a71dedd4e65fe03fe1e75a480c4b2708a5e559cc
Guilherme <guilherme.passos2003@hotmail.com>
2024-05-19 19:49:12 -0300

    removendo prettier e eslintrc

--- src/modules/fazendas/dto/update-fazenda.dto.ts
+++ src/modules/fazendas/dto/update-fazenda.dto.ts
@@ -1,4 +1,1 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateFazendaDto } from './create-fazenda.dto';
-
-export class UpdateFazendaDto extends PartialType(CreateFazendaDto) {}
+export class UpdateFazendaDto {}
